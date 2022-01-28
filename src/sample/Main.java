4c97d727bcda24e0b8427e213f2d2a95ff1271ad
strub17u <strub17u@etu.univ-lorraine.fr>
2022-01-28 11:07:11 +0100

    Premier commit marche s'il te plaît

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        BorderPane root = new BorderPane();
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
