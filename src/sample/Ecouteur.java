cd3b99805783a3b5258e21002a6d9e126af480de
strub17u <strub17u@etu.univ-lorraine.fr>
2022-01-28 11:49:17 +0100

    Bouton ==> Message de bienvenue + compteur

--- src/sample/Ecouteur.java
+++ src/sample/Ecouteur.java
@@ -4,11 +4,16 @@ import javafx.event.EventHandler;
 
 public class Ecouteur implements EventHandler<ActionEvent> {
 
+    int compteur;
+
     public Ecouteur(){
+        compteur = 0;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.out.println("Bienvenito ! Ce qui en italien veut dire : Bienvenue !!");
+        System.out.println("Bienvenito ! Ce qui en italien signifie : Bienvenue !!");
+        compteur++;
+        System.out.println("Vous avez appuyé " + compteur +"x sur le bouton !\n");
     }
 }
