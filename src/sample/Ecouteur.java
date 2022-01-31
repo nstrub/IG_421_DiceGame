5e53fecda1a69b67df2477a7722eb76903824600
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-01-31 12:05:59 +0100

    Projet sur perso + val aléatoires sur label

--- src/sample/Ecouteur.java
+++ src/sample/Ecouteur.java
@@ -1,13 +1,21 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+
 
 public class Ecouteur implements EventHandler<ActionEvent> {
 
     private int compteur;
+    private Label lab1;
+    private Label lab2;
+    private Label lab3;
 
-    public Ecouteur(){ //3 label en entre pour la question 5 ? Random 1-6
+    public Ecouteur(Label l1, Label l2, Label l3){ //3 label en entre pour la question 5 ? Random 1-6
         compteur = 0;
+        lab1 = l1;
+        lab2 = l2;
+        lab3 = l3;
     }
 
     @Override
@@ -15,5 +23,11 @@ public class Ecouteur implements EventHandler<ActionEvent> {
         System.out.println("Bienvenito ! Ce qui en italien signifie : Bienvenue !!");
         compteur++;
         System.out.println("Vous avez appuyé " + compteur +"x sur le bouton !\n");
+        int nb1 = (int)(Math.random()*6) + 1;
+        int nb2 = (int)(Math.random()*6) + 1;
+        int nb3 = (int)(Math.random()*6) + 1;
+        lab1.setText(String.valueOf(nb1));
+        lab2.setText(String.valueOf(nb2));
+        lab3.setText(String.valueOf(nb3));
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -23,16 +24,13 @@ public class Main extends Application {
         primaryStage.show();
         Button boutonJouer = new Button("Jouer");
         root.setBottom(boutonJouer);
-        boutonJouer.setOnAction(new Ecouteur());
-        javafx.scene.control.Label l1 = new javafx.scene.control.Label("1");
-        javafx.scene.control.Label l2 = new javafx.scene.control.Label("1");
-        javafx.scene.control.Label l3 = new javafx.scene.control.Label("1");
-        HBox boite = new HBox();
+        Label l1 = new Label("1");
+        Label l2 = new Label("1");
+        Label l3 = new Label("1");
+        boutonJouer.setOnAction(new Ecouteur(l1,l2,l3));
+        HBox boite = new HBox(l1,l2,l3);
         boite.setAlignment(Pos.CENTER);
-        boite.getChildren().addAll(l1,l2,l3);
         root.setCenter(boite);
-        
-
     }
 
 
