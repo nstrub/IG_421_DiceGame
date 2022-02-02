f32c5ba45b1a46d01a884c72efdba757f105c449
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-02-02 17:30:04 +0100

    Dés au milieu aléatoire

--- src/sample/Ecouteur.java
+++ src/sample/Ecouteur.java
@@ -2,6 +2,8 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class Ecouteur implements EventHandler<ActionEvent> {
@@ -28,15 +30,31 @@ public class Ecouteur implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        //Partie terminal
         System.out.println("Bienvenito ! Ce qui en italien signifie : Bienvenue !!");
         compteur++;
         System.out.println("Vous avez appuyé " + compteur +"x sur le bouton !\n");
+
+        //Setup aléatoire des dés
         int nb1 = (int)(Math.random()*6) + 1;
         int nb2 = (int)(Math.random()*6) + 1;
         int nb3 = (int)(Math.random()*6) + 1;
-        lab1.setText(String.valueOf(nb1));
-        lab2.setText(String.valueOf(nb2));
-        lab3.setText(String.valueOf(nb3));
+        String strD1 = "ressources/dice_"+ nb1 +".png";
+        String strD2 = "ressources/dice_"+ nb2 +".png";
+        String strD3 = "ressources/dice_"+ nb3 +".png";
+        Image imgD1 = new Image(strD1,60,60,true,true);
+        ImageView d1 = new ImageView(imgD1);
+
+        //Partie dés
+        lab1.setGraphic(d1);
+        Image imgD2 = new Image(strD2,60,60,true,true);
+        ImageView d2 = new ImageView(imgD2);
+        lab2.setGraphic(d2);
+        Image imgD3 = new Image(strD3,60,60,true,true);
+        ImageView d3 = new ImageView(imgD3);
+        lab3.setGraphic(d3);
+
+        //Partie haute
         labCompteur.setText(String.valueOf("Nb press bouton : " + compteur + " /"));
         if(nb1 == 4 && nb2 == 2 && nb3 == 1){
             nbCombi++;
